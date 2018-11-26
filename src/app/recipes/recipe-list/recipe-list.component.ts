297c558ff5102f23ba43f93cc6676e1643f9bb86
Samuel Pedro <samuelcpedro@gmail.com>
2018-11-26 22:07:18 -0100

    go
    Go

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  public recipes: Recipe[] = [
+    // tslint:disable-next-line:max-line-length
+    new Recipe('A Test Recipe',
+    'This is simply a test',
+    'https://www.maxpixel.net/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
+    new Recipe('A Test Recipe',
+     'This is simply a test',
+     'https://www.maxpixel.net/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
+     new Recipe('A Test Recipe',
+      'This is simply a test',
+      'https://www.maxpixel.net/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
+      new Recipe('A Test Recipe',
+       'This is simply a test',
+       'https://www.maxpixel.net/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
+       new Recipe('A Test Recipe',
+        'This is simply a test',
+        'https://www.maxpixel.net/static/photo/1x/Meat-Power-Recipe-Food-Dishes-Pork-1459693.jpg'),
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
