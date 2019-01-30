3849a094dc97c0ee8ae85302d80d88f65880ee4e
Samuel Pedro <samuelcpedro@gmail.com>
2019-01-30 22:27:43 -0100

    Adding Actions
    Adding Actions
    Section 24, Lecture 317

--- src/app/shopping-list/store/shopping-list.reducers.ts
+++ src/app/shopping-list/store/shopping-list.reducers.ts
@@ -1,6 +1,6 @@
 import { Action } from '@ngrx/store';
 
-import { Ingredient } from '../shared/ingredient.model';
+import { Ingredient } from '../../shared/ingredient.model';
 
 export const ADD_INGREDIENT = 'ADD_INGREDIENT';
 
