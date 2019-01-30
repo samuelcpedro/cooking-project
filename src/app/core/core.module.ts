eddcd53d8c0b24a3372b643c63568687504f571c
Samuel Pedro <samuelcpedro@gmail.com>
2019-01-29 23:25:29 -0100

    Modifying Requests in Interceptors
    Modifying Requests in Interceptors
    Section 23, Lecture 310

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -8,6 +8,7 @@ import { DataStorageService } from '../shared/data-storage.service';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { AppRoutingModule } from './../app-routing.module';
 import { AuthInterceptor } from './../shared/auth.interceptor';
+import { LoggingInterceptor } from './../shared/logging.interceptor';
 import { SharedModule } from './../shared/shared.module';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
@@ -32,7 +33,8 @@ import { HomeComponent } from './home/home.component';
     RecipeService,
     DataStorageService,
     AuthService,
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true},
+    {provide: HTTP_INTERCEPTORS, useClass: LoggingInterceptor, multi: true}
   ]
 })
 export class CoreModule { }
---
+++ src/app/shared/logging.interceptor.ts
@@ -0,0 +1,15 @@
+import { HttpInterceptor, HttpEvent, HttpRequest, HttpHandler } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { tap } from 'rxjs/operators';
+
+export class LoggingInterceptor implements HttpInterceptor {
+
+  constructor() {  }
+
+  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return next.handle(req).pipe(tap(event => {
+        console.log('Logging interceptor', event);
+      }
+    ));
+  }
+}
