bee1efa51761f5eca271d5a198c3021db3e49bf4
Samuel Pedro <samuelcpedro@gmail.com>
2019-01-29 23:04:06 -0100

    Interceptors
    Interceptors
    Section 23, Lecture 308

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,4 +1,5 @@
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AuthService } from '../auth/auth.service';
@@ -6,9 +7,11 @@ import { RecipeService } from '../recipes/recipe.service';
 import { DataStorageService } from '../shared/data-storage.service';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { AppRoutingModule } from './../app-routing.module';
+import { AuthInterceptor } from './../shared/auth.interceptor';
 import { SharedModule } from './../shared/shared.module';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
+
 
 @NgModule({
   imports: [
@@ -29,6 +32,7 @@ import { HomeComponent } from './home/home.component';
     RecipeService,
     DataStorageService,
     AuthService,
+    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
   ]
 })
 export class CoreModule { }
